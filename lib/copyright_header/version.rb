3f3f5ea3304db171b9ae673455121bf5a5fff02b
Erik Osterman <e@osterman.com>
2016-12-12 22:48:03 -0800

    Bump to 1.0.20

--- lib/copyright_header/version.rb
+++ lib/copyright_header/version.rb
@@ -1,22 +1,22 @@
 #
 # Copyright Header - A utility to manipulate copyright headers on source code files
-# Copyright (C) 2012 Erik Osterman <e@osterman.com>
-# 
+# Copyright (C) 2012-2016 Erik Osterman <e@osterman.com>
+#
 # This file is part of Copyright Header.
-# 
+#
 # Copyright Header is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # Copyright Header is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with Copyright Header.  If not, see <http://www.gnu.org/licenses/>.
 #
 module CopyrightHeader
-  VERSION = "1.0.19"
+  VERSION = "1.0.20"
 end
